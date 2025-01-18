7120f2bd27408295ce537130f2b9833873eb1c7a
mahmoud-ramadn <mr9026573@gmail.com>
2025-01-18 23:30:01 +0200

    flex-import

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,6 +7,8 @@ import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
 import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
 import exploremv from "/assets/videos/explore.mp4";
 import framemv from "/assets/videos/frame.mp4";
+import hero from "/assets/images/hero.jpeg"; // Ensure this path is correct and matches the file location.
+
 
 import apple from "/assets/images/apple.svg";
 import search from "/assets/images/search.svg";
